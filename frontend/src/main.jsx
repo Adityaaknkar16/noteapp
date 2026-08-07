6761786fe7b13cc005d33289aaca72a1e4484976
Aditya Akankar <adityaakankar@gmail.com>
2026-08-07 20:05:57 +0530

    refactor: support VITE_API_URL environment variable for API base URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 import {Provider} from "react-redux"
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 axios.defaults.withCredentials = true;
 import { persistor, store } from './redux/userslice/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
